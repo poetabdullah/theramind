51b1fdb0e5ddd6cac60e0421129b48cdc51822d3
Zeeshan0495 <zeeshan04956@gmail.com>
2025-01-17 20:42:56 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import QuestionnairePage from './pages/QuestionnairePage';
 import Navbar from './components/Navbar';
+import DiagnosticQuestionnaire from './components/DiagnosticQuestionnaire';
 
 const App = () => (
   <div>
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/questionnaire" element={<DiagnosticQuestionnaire />} />
     </Routes>
-    <Routes>
-      <Route path="/" element={<QuestionnairePage />} />
-    </Routes>
+
   </div>
 );
 
