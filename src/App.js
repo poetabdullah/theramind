0217bb1b6cfd3293c0bfde2b991df4e1170899a9
Zeeshan0495 <zeeshan04956@gmail.com>
2025-01-17 15:13:27 +0500

    Questionnaire page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Questionnaire from './pages/Questionnaire';
 import Navbar from './components/Navbar';
 
 const App = () => (
   <div>
     <Navbar />
     <Routes>
-      <Route path="/" element={<HomePage />} />      
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    <Routes>
+      <Route path="/" element={<Questionnaire />} />
     </Routes>
   </div>
 );
---
+++ src/pages/Questionnaire.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import Services from '../components/Services';
+import HowItWorks from '../components/HowItWorks';
+import Testimonial from '../components/Testimonial';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+const Questionnaire = () => (
+  <>
+    <Banner />
+    <Services />
+    <HowItWorks />
+    <Testimonial />
+    <Contact />
+    <Footer />
+  </>
+);
+export default Questionnaire;
