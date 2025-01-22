febeb4bc805ff840eaf2116ac0a2d14f1ef6a166
Abdllah Imran <abdullahimranarshad@gmail.com>
2025-01-22 16:47:02 +0500

    Solved merged conflict???

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Navbar from './components/Navbar';
-import QuestionnaireForm from './pages/Questionnaire';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import QuestionnaireForm from "./pages/Questionnaire";
 
 const App = () => (
   <div>
