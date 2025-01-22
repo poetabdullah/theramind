16346f6bc6e99e498bc546cbfc2579f0f1f5b98e
Abdllah Imran <abdullahimranarshad@gmail.com>
2025-01-22 16:40:10 +0500

    nice

--- src/pages/questionnaire.js
+++ src/pages/questionnaire.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Footer from '../components/Footer';
+import React from "react";
+import Footer from "../components/Footer";
 
 const Questionnaire = () => (
   <>
-    <div className='items-center'>
+    <div className="items-center">
       <div>
-        <h2 className='text-customPurple shadow-md rounded-lg max-w-xl mx-auto'>Questionnaire</h2>
+        <h2 className="text-customPurple shadow-md rounded-lg max-w-xl mx-auto">
+          Questionnaire
+        </h2>
       </div>
       <div>
         <h3>Q1. Have you been feeling any of the following lately?</h3>
