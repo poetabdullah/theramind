351774c11baf741d52c464c402422e4b8f640452
Abdllah Imran <abdullahimranarshad@gmail.com>
2025-01-22 16:47:02 +0500

    Solved merged conflict???

--- src/pages/questionnaire.js
+++ src/pages/questionnaire.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
-
-const Questionnaire = () => (
-  <>
-    <div className="items-center">
-      <div>
-        <h2 className="text-customPurple shadow-md rounded-lg max-w-xl mx-auto">
-          Questionnaire
-        </h2>
-      </div>
-      <div>
-        <h3>Q1. Have you been feeling any of the following lately?</h3>
-      </div>
-    </div>
-=======
-import React, { useState } from 'react';
-import Footer from '../components/Footer';
 
 const QuestionnaireForm = () => {
   const [responses, setResponses] = useState({
@@ -27,7 +10,6 @@ const QuestionnaireForm = () => {
     flashbacksNightmares: false,
     none: false,
   });
->>>>>>> 0be406f73d3af436e3d50de1675519bf96ac670f
 
   const changeEvent = (event) => {
     const { name, checked } = event.target;
@@ -36,49 +18,88 @@ const QuestionnaireForm = () => {
       [name]: checked,
     }));
   };
-  return (<>
-    <div>
-      <h2 className='text-customPurple shadow-md rounded-lg max-w-xl mx-auto'>Questionnaire</h2>
-    </div>
-    <div>
-      <h3>Q1. Have you been feeling any of the following lately?</h3>
-    </div>
-    <div>
-      <form>
-        <label>
-          <input type="checkbox" name="depressedLonely" checked={responses.depressedLonely}
-            onChange={changeEvent}>Feeling depressed & lonely</input>
-        </label>
-        <br></br>
-        <label>
-          <input type="checkbox" name="lossofInterest" checked={responses.lossofInterest}
-            onChange={changeEvent}>Loss of interest in activities</input>
-        </label>
-        <br></br>
-        <label>
-          <input type="checkbox" name="repetitiveBehavior" checked={responses.repetitiveBehavior}
-            onChange={changeEvent}>Repetitive behaviors</input>
-        </label>
-        <br></br>
-        <label>
-          <input type="checkbox" name="difficultyBreathing" checked={responses.difficultyBreathing}
-            onChange={changeEvent}>Difficulty concentrating</input>
-        </label>
-        <br></br>
-        <label>
-          <input type="checkbox" name="flashbacksNightmares" checked={responses.flashbacksNightmares}
-            onChange={changeEvent}>Flashbacks/nightmares</input>
-        </label>
-        <br></br>
-        <label>
-          <input type="checkbox" name="none" checked={responses.none}
-            onChange={changeEvent}>None of the above</input>
-        </label>
-        <br></br>
-      </form>
-    </div>
-    <Footer />
-  </>
+  return (
+    <>
+      <div>
+        <h2 className="text-customPurple shadow-md rounded-lg max-w-xl mx-auto">
+          Questionnaire
+        </h2>
+      </div>
+      <div>
+        <h3>Q1. Have you been feeling any of the following lately?</h3>
+      </div>
+      <div>
+        <form>
+          <label>
+            <input
+              type="checkbox"
+              name="depressedLonely"
+              checked={responses.depressedLonely}
+              onChange={changeEvent}
+            >
+              Feeling depressed & lonely
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              name="lossofInterest"
+              checked={responses.lossofInterest}
+              onChange={changeEvent}
+            >
+              Loss of interest in activities
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              name="repetitiveBehavior"
+              checked={responses.repetitiveBehavior}
+              onChange={changeEvent}
+            >
+              Repetitive behaviors
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              name="difficultyBreathing"
+              checked={responses.difficultyBreathing}
+              onChange={changeEvent}
+            >
+              Difficulty concentrating
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              name="flashbacksNightmares"
+              checked={responses.flashbacksNightmares}
+              onChange={changeEvent}
+            >
+              Flashbacks/nightmares
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              name="none"
+              checked={responses.none}
+              onChange={changeEvent}
+            >
+              None of the above
+            </input>
+          </label>
+          <br></br>
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 };
 export default QuestionnaireForm;
