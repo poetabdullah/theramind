7aba1af8ff76e15bf2eea0b0e514c8949a7fbba1
Ambreenakhtar04 <ambreenambi04@gmail.com>
2025-01-25 09:37:33 +0500

    checking sign up!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js//bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
