a7e456771ac33ede86f2b072afbeb93d5427868a
Abdllah Imran <abdullahimranarshad@gmail.com>
2025-03-11 18:57:09 +0500

    qasam us waqt ki, kasam us waqt ki, jab waqt waqt ko azmata hai

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,4 +1,4 @@
-// TheraChat welcome/splash screen
+// TheraChat welcome/splash Screen
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
