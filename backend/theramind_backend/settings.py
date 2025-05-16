f524734191ce9f5366704aa36f67fb089f026d82
Ambreenakhtar04 <ambreenambi04@gmail.com>
2025-05-16 07:26:02 +0500

    Removed The Backend Code!

--- backend/theramind_backend/settings.py
+++ backend/theramind_backend/settings.py
@@ -202,11 +202,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-#Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'theramind.web@gmail.com'
-EMAIL_HOST_PASSWORD = ''
