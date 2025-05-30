2a5bd632f9f027581cea0134b7b36b47c5c7944c
Ambreenakhtar04 <ambreenambi04@gmail.com>
2025-05-30 08:05:04 +0500

    Pulling.

--- src/components/AppointmentConfirmationEmail.js
+++ src/components/AppointmentConfirmationEmail.js
@@ -1,24 +1,24 @@
 import emailjs from 'emailjs-com';
 
 export const sendAppointmentConfirmationEmail = ({
-  patient_name,
-  doctor_name,
-  time,
-  meet_link,
-  email,
+  patientName,
+  doctorName,
+  timeslot,
+  meetLink,
+  patientEmail,
 }) => {
   try {
     emailjs.send(
-      'theramind_service',             
-      'confirmation_template',           
+      'theramind_service',
+      'confirmation_template',
       {
-        patient_name,
-        doctor_name,
-        time,
-        meet_link,
-        email,                         
+        patient_name: patientName,
+        doctor_name: doctorName,
+        timeslot: new Date(timeslot).toLocaleString(),
+        meet_link: meetLink,
+        email: patientEmail,
       },
-      'TA0YUZ8PvIadVetlu'                
+      'TA0YUZ8PvIadVetlu'
     ).then(
       response => {
         console.log('Email sent successfully:', response.status, response.text);
