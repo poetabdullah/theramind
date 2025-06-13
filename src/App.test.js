5d739a3bdf5b3681b2ec2fd12cad7e5639a1c100
hamdaqad <hamdaq2002@gamil.com>
2025-06-13 22:48:55 +0500

    block patient only static mails rn

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+function App() {
+  return (
+    <>
+      
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
+  );
+}
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+export default App;
