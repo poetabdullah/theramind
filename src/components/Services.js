8f40dcf15e9b067b4cff451c4940519d97cca2b7
hamdaqad <hamdaq2002@gamil.com>
2025-06-30 02:12:35 +0500

    fixed issues

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,19 +8,19 @@ const Services = () => (
       <div className="service-card">
         <h3>Schedule Virtual Appointments</h3>
         <p>You can schedule virtual appointments with licensed clinical psychologists on TheraMind, who will guide you in your self-healing journey.</p>
-        <button className="service-btn">Book a Session</button>
+        
       </div>
       
       <div className="service-card">
         <h3>Get Personalized Treatment Plan</h3>
         <p>Based on assessment by the doctors, you will have an easy to follow daily actionable treatment plan that will track your healing journey.</p>
-        <button className="service-btn">Get yourself better</button>
+     
       </div>
       
       <div className="service-card">
         <h3>Keep Track of your Journey</h3>
         <p>Stay on track with our progress tracker that that will support your healing journey. <br></br>It will provide you with updated progress of your treatment plan.</p>
-        <button className="service-btn">See progress status</button>
+        
       </div>
       
     </div>
